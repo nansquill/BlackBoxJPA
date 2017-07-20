1fb14ea86967aa4d78f45522f35bebce9714565b
neqr <neqr@2die4.com>
2017-07-20 11:37:38 +0200

    Alle Nachrichten einer Kategorie können nun über rest/types/{name}/messages erhalten werden

--- blackbox-business/src/main/java/com/webtec2/CategoriesCRUD.java
+++ blackbox-business/src/main/java/com/webtec2/CategoriesCRUD.java
@@ -1,29 +1,15 @@
 package com.webtec2;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Order;
-import javax.persistence.criteria.Root;
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException;
-import java.lang.IllegalStateException;
-import java.lang.IllegalArgumentException;
-import javax.persistence.TransactionRequiredException;
+import javax.persistence.*;
+import javax.persistence.criteria.*;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+import javax.ws.rs.*;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import java.util.Date;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 @Path("/types")
 @Transactional
@@ -67,24 +53,6 @@ public class CategoriesCRUD {
 		return result;	
 	}
 
-	/*
-	@Path("/{id}")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public DBCategory read(@PathParam("id") final long id) {
-		DBCategory category = null;
-		try
-		{
-			category =  this.entityManager.find(DBCategory.class, id);
-		}
-		catch(IllegalArgumentException ex)
-		{
-			//if the first argument does not denote an entity type or the second argument is is not a valid type for that entity primary key or is null
-		}
-		return category;
-	}
-	 */
-
 	@Path("/{name}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -103,6 +71,17 @@ public class CategoriesCRUD {
 			//if the first argument does not denote an entity type or the second argument is is not a valid type for that entity primary key or is null
 		}
 		return category;
+	}
+
+	@Path("/{name}/messages")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<DBMessage> getMessages(@PathParam("name") String name) {
+		DBCategory category = read(name);
+		Query query = this.entityManager.createQuery("SELECT m FROM DBMessage m JOIN m.category c WHERE c = :category");
+		query.setParameter("category", category);
+
+		return (List<DBMessage>) query.getResultList();
 	}
 
 	@POST
